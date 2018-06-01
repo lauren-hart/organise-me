a223ef9ae5c293773b5c9e97e64705f2985db9ad
Emily Parkes <emilycoco@me.com>
2018-06-01 15:39:31 +1200

    basic component

---
+++ client/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className='page-section'>
+      
+        <div className='page-heading-section'>
+          <div className='page-title'>
+            <h3>about</h3>
+          </div>
+        </div>
+
+        <div className='page-content'>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default About
