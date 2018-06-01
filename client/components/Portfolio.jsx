1e03029e4109423df27fb9fe1376b91a76d60041
Emily Parkes <emilycoco@me.com>
2018-06-01 15:40:39 +1200

    basic component

---
+++ client/components/Portfolio.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <div className='page-section'>
+      
+        <div className='page-heading-section'>
+          <div className='page-title'>
+            <h3>portfolio</h3>
+          </div>
+        </div>
+
+        <div className='page-content'>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default Portfolio
