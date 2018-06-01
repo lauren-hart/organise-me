e76586c9b596c065ede721597bb504d9663dc57d
Emily Parkes <emilycoco@me.com>
2018-06-01 15:40:10 +1200

    basic component

---
+++ client/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className='page-section'>
+      
+        <div className='page-heading-section'>
+          <div className='page-title'>
+            <h3>contact</h3>
+          </div>
+        </div>
+
+        <div className='page-content'>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default Contact
