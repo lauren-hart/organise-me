cb7cbc66162d66a91639db5d03364e02f2d63bf4
Emily Parkes <emilycoco@me.com>
2018-06-01 15:41:49 +1200

    add component routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,9 @@ import { Route, Switch } from 'react-router-dom'
 
 import Header from './Header'
 import Home from './Home'
+import Portfolio from './Portfolio'
+import About from './About'
+import Contact from './Contact'
 import Footer from './Footer'
 
 class App extends React.Component {
@@ -15,6 +18,9 @@ class App extends React.Component {
         <div className='app-box'>
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
           </Switch>
         </div>
         
