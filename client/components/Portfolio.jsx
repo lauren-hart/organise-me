c31e0c5dd1568c9c333560682ecffdec95aa0284
Emily Parkes <emilycoco@me.com>
2018-06-04 11:51:12 +1200

    update

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -4,7 +4,7 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div className='page-section'>
-      
+
         <div className='page-heading-section'>
           <div className='page-title'>
             <h3>portfolio</h3>
@@ -12,6 +12,41 @@ class Portfolio extends React.Component {
         </div>
 
         <div className='page-content'>
+          <div className='image-gallery'>
+            <div class='img-row'>
+              <div class='img-column'>
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+              </div>
+
+              <div class='img-column'>
+              <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+              </div>
+
+              <div class='img-column'>
+              <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+                <img src='images/graphic.jpg' />
+              </div>
+
+            </div>
+          </div>
         </div>
 
       </div>
