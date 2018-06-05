c03ca52fc4e8f6a377039467c8845f72a92f69b7
Emily Parkes <emilycoco@me.com>
2018-06-05 15:37:31 +1200

    change layout style of works to one column

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -12,44 +12,49 @@ class Portfolio extends React.Component {
         </div>
 
         <div className='page-content'>
-          <div className='image-gallery'>
-            <div class='img-row'>
-              <div class='img-column'>
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
+
+          <div id='marama' className='work-shots'>
+            <div className='container'>
+              <div className='shot'>
+                <img src='images/marama-darkest.png' /></div>
+              <div className='shot-info'>
+                <h2>Mārama</h2>
+                <h3>Full Stack Development</h3>
+                <p>Mārama is a web app I created as part of a final project at EDA. It's purpose is to ....</p>
               </div>
+            </div >
+          </div >
 
-              <div class='img-column'>
-              <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
+          <div id='work2' className='work-shots'>
+            <div className='container'>
+              <div className='shot'>
+                <img src='images/graphic.jpg' /></div>
+              <div className='shot-info'>
+                <h2>Work 2</h2>
+                <h3>Full Stack Development</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut blandit nunc. Donec at enim tortor. Nullam ultricies porttitor velit, eget feugiat neque tempus eu. Vestibulum euismod consectetur magna. Vestibulum enim massa, accumsan et hendrerit sit amet, elementum nec urna. Nam vestibulum rhoncus orci, quis congue justo tempor sit amet. Vivamus egestas quam non congue pellentesque. Mauris in egestas urna. Phasellus velit quam, facilisis vitae enim ac, vestibulum suscipit enim.</p>
               </div>
+            </div >
+          </div >
 
-              <div class='img-column'>
-              <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
-                <img src='images/graphic.jpg' />
+          <div id='work3' className='work-shots'>
+            <div className='container'>
+              <div className='shot'>
+                <img src='images/graphic.jpg' /></div>
+              <div className='shot-info'>
+                <h2>Work 3</h2>
+                <h3>Full Stack Development</h3>
+                <p>Fusce turpis tortor, tincidunt eget dictum vel, ullamcorper id nunc. Etiam vestibulum ullamcorper euismod. Suspendisse sed lacus at nunc consectetur rhoncus vitae ac lacus. Pellentesque quis laoreet augue. Nulla eget imperdiet magna. Curabitur efficitur semper enim eget ultrices. Vivamus risus neque, pretium id egestas eu, consequat vitae purus. Aliquam ut leo vel neque viverra finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras id metus ex.</p>
               </div>
+            </div >
+          </div >
 
-            </div>
-          </div>
-        </div>
+          {/* END-page-content */}
+        </div >
 
+        {/* END-page-section */}
       </div>
+
     )
   }
 }
