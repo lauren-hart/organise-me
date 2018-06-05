b91e47ad0ea31e356c5f5a2fc77876f226bcfe8f
Emily Parkes <emilycoco@me.com>
2018-06-05 15:40:18 +1200

    insert and link logo to home

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Nav from './Nav'
 
 const Header = () => {
   return (
-    <div className='header-box'>
-      <div className='header-content'>
-      <Nav />
+    <div className='header-content'>
+      <div className='logo'>
+        <Link to='/'>
+          <img src='/images/logo.png' className='logo' />
+        </Link>
       </div>
-    </div>
-  )
-}
-
+        <Nav />
+      </div>
+      )
+    }
+    
 export default Header
