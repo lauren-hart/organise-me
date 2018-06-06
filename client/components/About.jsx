081cbc50c8f710762709d2716d8d2071745f3051
Emily Parkes <emilycoco@me.com>
2018-06-06 15:44:54 +1200

    consist styles naming

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 class About extends React.Component {
   render() {
     return (
-      <div className='page-section'>
+      <div className='about'>
 
         <div className='page-content'>
           <div className='page-blurb'>
