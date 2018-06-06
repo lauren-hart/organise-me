6a9dceb6f1515cef2c0fd00609ef3a57120a2603
Emily Parkes <emilycoco@me.com>
2018-06-07 11:01:58 +1200

    bootstrap nav styles

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div className='nav'>
-      <div className='nav-links'>
-        <span className='nav-link-individual-styling'>
+      <nav className='nav justify-content-end flex-column flex-sm-row'>
+        <span className='flex-sm-fill text-sm-center nav-link active'>
           <Link to='/about'>about</Link>
         </span>
-        <span className='nav-link-individual-styling'>
+        <span className='flex-sm-fill text-sm-center nav-link'>
           <Link to='/portfolio'>portfolio</Link>
         </span>
-      </div>
-    </div>
+      </nav>
   )
 }
 
 export default Nav
+
+
+{/* <nav class='nav nav-pills flex-column flex-sm-row'>
+  <a class='flex-sm-fill text-sm-center nav-link active' href='#'>Active</a>
+  <a class='flex-sm-fill text-sm-center nav-link' href='#'>Link</a>
+  <a class='flex-sm-fill text-sm-center nav-link' href='#'>Link</a>
+  <a class='flex-sm-fill text-sm-center nav-link disabled' href='#'>Disabled</a>
+</nav> */}
