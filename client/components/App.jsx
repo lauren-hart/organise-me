30ba2c0657468f3236195c523125a6efba025689
Emily Parkes <emilycoco@me.com>
2018-06-08 16:26:46 +1200

    remove unused div

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,13 +12,11 @@ class App extends React.Component {
     return (
       <div className='app'>
         <Header />
-
-        <div className='app-box'>
+        
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/portfolio' component={Portfolio} />
           </Switch>
-        </div>
 
         <Footer />
       </div>
