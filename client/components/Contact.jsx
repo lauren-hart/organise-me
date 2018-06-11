6cf72546d4c914c3fe17331640774b02f452a0b2
Emily Parkes <emilycoco@me.com>
2018-06-11 14:27:22 +1200

    add into own component

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -3,13 +3,29 @@ import React from 'react'
 class Contact extends React.Component {
   render() {
     return (
-      <div className='page-section'>
+      <section id='message-me'>
 
-        <div className='page-content'>
-          {/* email section here */}
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <div className='chat-blurb'>
+                <div className='title'><h2>Would you like to chat?</h2></div>
+
+                <p>Send me an email and we can talk.</p>
+
+                <p className='btn-hldr'>
+                  <a href='mailto:emilycoco@me.com' className='main-btn email-lnk'>
+                    <img className='email-img' src='/icons/email-icon.png' height='20px' />
+                    {' '} {' '} message me
+                    </a>
+                </p>
+
+              </div>
+            </div>
+          </div>
         </div>
 
-      </div>
+      </section>
     )
   }
 }
