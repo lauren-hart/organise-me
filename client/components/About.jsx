1f847be13af2c97c4b317855660935b9e060439d
Emily Parkes <emilycoco@me.com>
2018-06-11 17:03:27 +1200

    update about me text

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 class About extends React.Component {
   render() {
     return (
-        <div className='page-content'>
+      <div className='page-content'>
 
-          <div className='page-about-blurb'>
-            <p>I'm</p>
-            <p>emily coco</p>
-            <p>a full stack web developer</p>
-          </div>
-
-          <div className='page-hello'>
-            <p>Heya!.</p>
-            <p>I'm a web developer based in Auckland.</p>
-            <p>I love to create web apps and broaden my skill set.</p>
-          </div>
-
+        <div className='page-about-blurb'>
+          <p>Heya!</p>
+          <p>I'm a web developer based in Auckland, New Zealand.</p>
+          <p>I love to create with code and broaden my skill set.</p>
         </div>
+
+        <div className='page-experience'>
+          <p>I have experience in Full Stack Development,</p>
+          <p>graduating from Enspiral Dev Academy in March 2018.</p>
+          <p>I have also been working for the company as a Teacher Assist and working on my own projects.</p>
+        </div>
+
+      </div>
     )
   }
 }
