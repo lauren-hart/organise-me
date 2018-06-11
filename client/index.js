6221c312da2c529f1cc3b319a425ed896b7409ec
Emily Parkes <emilycoco@me.com>
2018-06-12 10:57:06 +1200

    remove redux

--- client/index.js
+++ client/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunkMiddleware from 'redux-thunk'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import App from './components/App'
-let store = createStore(compose(
-  applyMiddleware(thunkMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
