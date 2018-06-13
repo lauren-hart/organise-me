3fa5c2027191d0e7af7d6394977b06644109268a
Emily Parkes <emilycoco@me.com>
2018-06-13 15:15:23 +1200

    alter text and style

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 class About extends React.Component {
   render() {
     return (
-      <div className='page-content'>
+      <div className='page-about'>
 
-        <div className='page-about-blurb'>
-          <p>Heya!</p>
-          <p>I'm a web developer based in Auckland, New Zealand.</p>
-          <p>I love to create with code and broaden my skill set.</p>
+        <div className='about-blurb'>
+          <h2>Hello!</h2>
+          <p>
+            I'm a web developer based in Auckland, New Zealand. <br/>
+            I love to create with code, and learning to broaden my skill set.
+            </p>
         </div>
 
-        <div className='page-experience'>
-          <p>I have experience in Full Stack Development,</p>
-          <p>graduating from Enspiral Dev Academy in March 2018.</p>
-          <p>I have also been working for the company as a Teacher Assist and working on my own projects.</p>
+        <div className='experience-blurb'>
+          <h3>A little about my experience,</h3>
+          <p className='exp-1'>I have always loved being able to create projects hands on and found tech and code to be an exciting and rewarding way of doing so.</p>
+          <p>I pursued my interest in code at Enspiral Dev Academy and graduated as a full stack developer in March 2018.</p>
         </div>
 
       </div>
