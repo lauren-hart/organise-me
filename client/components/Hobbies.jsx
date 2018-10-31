a8e276c5335802f615cc95c7ec51544865a2fb2f
Lauren Hart <laurenannehart@gmail.com>
2018-11-01 07:55:32 +1300

    added golf picture in

---
+++ client/components/Hobbies.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+class Hobbies extends React.Component {
+  render () {
+    return (
+      <div className="hobbies">
+        <h3>Hobbies</h3>
+        <div className="golf">
+          <h5 className="golf-h5">golf</h5>
+        </div>
+      </div>
+    )
+  }
+}
+export default Hobbies
