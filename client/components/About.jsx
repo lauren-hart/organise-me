6bf91867e0a6e7bacf5240e29c65b2d3792ad96f
Lauren Hart <laurenannehart@gmail.com>
2018-10-31 21:09:52 +1300

    added a border to the about section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -4,8 +4,10 @@ class About extends React.Component {
   render () {
     return (
       <div className="about">
+      <div className="row">
+      <div className="col-md-6">
         <h5 className="about-h5">about me</h5>
-        <p>I am full stack web developer with a strong commercial
+        <p className="about-p">I am full stack web developer. <br /> I have a strong commercial
           background as a chartered accountant. < br />
           I have a love for using technology to automate and solve
           everyday tasks and problems. < br />
@@ -14,6 +16,8 @@ class About extends React.Component {
           I'm giving is making an impact on users. < br />
           I want to be part of a team that's proud of their work.< br />
         </p>
+      </div>
+      </div>
       </div>
     )
   }
