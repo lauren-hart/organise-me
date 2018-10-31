aaf7c347ae22e754775d1cf05363733bbc693f08
Lauren Hart <laurenannehart@gmail.com>
2018-10-31 15:30:00 +1300

    fixed the header and nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,16 +4,12 @@ import {Route, Switch} from 'react-router-dom'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
-import Nav from './Nav'
 
 class App extends React.Component {
   render () {
     return (
       <div className='app'>
-        <div className="header-group">
-          <Header />
-          <Nav />
-        </div>
+        <Header />
         <Switch>
           <Route exact path='/' component={Home} />
         </Switch>
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import Nav from './Nav'
 
 const Header = () => {
   return (
     <header className="container-fluid header">
       <h1 className="name-h1">Lauren Hart</h1>
       <h5 className="title-h5">full stack developer</h5>
+      <Nav />
     </header>
   )
 }
