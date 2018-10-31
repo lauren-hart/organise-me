683d15ea922d59c794ac5c706dc62045f6ddaaf8
Lauren Hart <laurenannehart@gmail.com>
2018-10-31 20:44:52 +1300

    added contact links

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
       <h1 className="name-h1">Lauren Hart</h1>
       <h5 className="title-h5">full stack developer</h5>
       <div className="row">
-        <div className="offset-by-two eight columns">
-          <a href="https://www.facebook.com/laurenannehart" className="fa fa-facebook"></a>
-          <a href="https://www.instagram.com/laurnhart/" className="fa fa-instagram"></a>
+        <div className="links">
+          <a href="mailto:laurenannehart@gmail.com" className="fa fa-envelope"></a>
+          <a href="https://github.com/lauren-hart" className="fa fa-github"></a>
           <a href="https://www.linkedin.com/in/lauren-hart-01551620/" className="fa fa-linkedin"></a>
         </div>
       </div>
