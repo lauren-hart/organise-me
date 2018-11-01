f5ad1328590d8242d2d7b0568089c80063a53e97
Lauren Hart <laurenannehart@gmail.com>
2018-11-01 18:17:08 +1300

    updated colour of headings

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,16 +4,19 @@ import React from 'react'
 class Nav extends React.Component {
   render () {
     return (
-      <div className="row nav">
-        <div className="col-md-4 about-nav nav-box">
-          <button className="nav-button">about</button>
+      <div>
+        <div className="row nav">
+          <div className="col-md-4 about-nav nav-box">
+            <button className="nav-button">about</button>
+          </div>
+          <div className="col-md-4 experience-nav nav-box">
+            <button className="nav-button">experience</button>
+          </div>
+          <div className="col-md-4 projects-nav nav-box">
+            <button className="nav-button">projects</button>
+          </div>
         </div>
-        <div className="col-md-4 experience-nav nav-box">
-          <button className="nav-button">experience</button>
-        </div>
-        <div className="col-md-4 projects-nav nav-box">
-          <button className="nav-button">projects</button>
-        </div>
+
       </div>
     )
   }
