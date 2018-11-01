0b717562b45c064c9e17fb3339ac1efc98c5463d
Lauren Hart <laurenannehart@gmail.com>
2018-11-02 10:49:49 +1300

    added link to button on nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 class Nav extends React.Component {
   render () {
     return (
       <div>
-        <div className="row nav">
+        <div className="nav row">
           <div className="col-md-4 about-nav nav-box">
-            <button className="nav-button">about</button>
+            <Link to ""><button className="nav-button">about</button></Link>
           </div>
           <div className="col-md-4 experience-nav nav-box">
             <button className="nav-button">experience</button>
