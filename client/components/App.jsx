ba539c1e25b8e2d16b4e754db8b39c8c37d765d8
Lauren Hart <laurenannehart@gmail.com>
2018-11-02 18:21:58 +1300

    made home a stateful component to try and render sections

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import {Route, HashRouter as Router} from 'react-router-dom'
-
+import {Switch, Route} from 'react-router-dom'
 import Home from './Home'
 
 class App extends React.Component {
   render () {
     return (
       <div className='app'>
-        <Router>
-          <div>
-            <Route exact path='/' component={Home}/>
-          </div>
-        </Router>
-
+        <Switch>
+          <Route exact path='/' component={Home}/>
+        </Switch>
       </div>
     )
   }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,20 +4,52 @@ import About from './About'
 import Stack from './Stack'
 import Experience from './Experience'
 import Projects from './Projects'
-import Footer from './Footer'
-import Header from './Header'
 
 class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      about: false,
+      exp: false,
+      projects: false
+    }
+    this.handleAbout = this.handleAbout.bind(this)
+  }
+
+  handleAbout () {
+    this.setState({
+      about: !this.state.about
+    })
+  }
+
   render () {
     return (
       <div>
-        <Header />
-        <About />
-        <Stack />
-        <Experience />
-        <Projects />
-        <Footer />
+        <header className="header">
+          <h1 className="name-h1">Lauren Hart</h1>
+          <h5 className="title-h5">full stack developer</h5>
+          <div className="row">
+            <div className="links">
+              <a href="mailto:laurenannehart@gmail.com" className="fa fa-envelope"></a>
+              <a href="https://github.com/lauren-hart" className="fa fa-github"></a>
+              <a href="https://www.linkedin.com/in/lauren-hart-01551620/" className="fa fa-linkedin"></a>
+            </div>
+          </div>
+
+          <div className="nav row">
+            <div className="col-md-4 about-nav nav-box">
+              <button onClick={this.handleClick} className="nav-button">about</button>
+            </div>
+            <div className="col-md-4 experience-nav nav-box">
+              <button className="nav-button">experience</button>
+            </div>
+            <div className="col-md-4 projects-nav nav-box">
+              <button className="nav-button">projects</button>
+            </div>
+          </div>
+        </header>
       </div>
+
     )
   }
 }
