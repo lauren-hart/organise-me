561a6261b90cd6a5f37043805a65dca774e39131
Lauren Hart <laurenannehart@gmail.com>
2018-11-03 08:52:18 +1300

    fixed 768px screen about me section

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import About from './About'
-import Stack from './Stack'
 import Experience from './Experience'
 import Projects from './Projects'
+import Stack from './Stack'
 
 class Home extends React.Component {
   constructor (props) {
@@ -14,11 +14,31 @@ class Home extends React.Component {
       projects: false
     }
     this.handleAbout = this.handleAbout.bind(this)
+    this.handleExp = this.handleExp.bind(this)
+    this.handleProjects = this.handleProjects.bind(this)
   }
 
   handleAbout () {
     this.setState({
-      about: !this.state.about
+      about: true,
+      exp: false,
+      projects: false
+    })
+  }
+
+  handleExp () {
+    this.setState({
+      exp: true,
+      about: false,
+      projects: false
+    })
+  }
+
+  handleProjects () {
+    this.setState({
+      projects: true,
+      about: false,
+      exp: false
     })
   }
 
@@ -38,16 +58,22 @@ class Home extends React.Component {
 
           <div className="nav row">
             <div className="col-md-4 about-nav nav-box">
-              <button onClick={this.handleClick} className="nav-button">about</button>
+              <button onClick={this.handleAbout} className="nav-button">about</button>
             </div>
-            <div className="col-md-4 experience-nav nav-box">
+            <div onClick={this.handleExp} className="col-md-4 experience-nav nav-box">
               <button className="nav-button">experience</button>
             </div>
-            <div className="col-md-4 projects-nav nav-box">
+            <div onClick={this.hanldeProjects} className="col-md-4 projects-nav nav-box">
               <button className="nav-button">projects</button>
             </div>
           </div>
         </header>
+
+        <About />
+        <Stack />
+        <Experience />
+        <Projects />
+
       </div>
 
     )
