7bb3cbfca6162348f3e98c37ece6064a36614663
Lauren Hart <laurenannehart@gmail.com>
2018-11-06 14:05:25 +1300

    fixed spacing on home page

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -9,12 +9,16 @@ class About extends React.Component {
         <div className="col-md-6">
           <h5 className="about-h5">about me</h5>
           <p className="about-p">
-                  I am full stack web developer. <br /> I have a strong commercial
-                  background as a chartered accountant. < br />
+                  I have recently graduated from <br />  
+            <a href="https://devacademy.co.nz/" className="contact-link">Enspiral Dev Academy</a>
+            .<br />
+                  I have a strong commercial
+                  background as a chartered accountant, but
+                  I promise I am not as boring as I sound! <br />
                   I have a love for using technology to automate and solve
                   everyday tasks and problems. < br />
-                  I am looking for a role that will challenge me and enable
-                  me to learn every day, <br /> where I can see that the input
+                  I am looking for a role that will challenge me <br /> and enable
+                  me to learn every day,  where I can see that the input
                   I'm giving is making an impact on users. < br />
                   I want to be part of a team that's proud of their work.< br />
           </p>
