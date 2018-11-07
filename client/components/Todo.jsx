ed05a109279f79f75b1e255448bff018e2f3ee3f
Lauren Hart <laurenannehart@gmail.com>
2018-11-08 12:35:57 +1300

    added a handlechange to input box

--- client/components/Todo.jsx
+++ client/components/Todo.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 // import {Link} from 'react-router-dom'
 
 class Todo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      input: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
   render () {
     return (
       <section className="todo">
         <h3 className="med-heading">to do</h3>
+        <input onChange={this.handleChange}></input>
       </section>
 
     )
