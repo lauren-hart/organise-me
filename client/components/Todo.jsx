f115c119bca1f97e93d54694530493d822d3d113
Lauren Hart <laurenannehart@gmail.com>
2018-11-08 12:18:50 +1300

    added header

---
+++ client/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import {Link} from 'react-router-dom'
+
+class Todo extends React.Component {
+  render () {
+    return (
+      <header>
+        <h1 className="organise-h1">organise me</h1>
+      </header>
+
+    )
+  }
+}
+export default Home
