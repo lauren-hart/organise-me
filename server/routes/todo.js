50cfd090aa7589c705ca94bf2937b0f5fdd7a7e6
Lauren Hart <laurenannehart@gmail.com>
2018-11-13 12:50:06 +1300

    added todo routes

---
+++ server/routes/todo.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const db = require('../db')
+const router = express.Router()
+router.use(express.json())
+
+// GET ROUTES (get todos)
+
+router.get('/', (req, res) => {
+  db.getTodos()
+    .then(result => {
+      res.json({result})
+    })
+})
+
+// DELETE ROUTES (delete todo by id)
+router.delete('/:id', (req, res) => {
+  const todoId = req.params.id
+  db.deleteTodos(todoId)
+    .then(() => {
+      res.json({})
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' +
+      err.message)
+    })
+})
+
+// POST ROUTES (add new todo)
+router.post('/addtodo', (req, res) => {
+  const todo = req.body
+  db.addTodo(todo)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' +
+      err.message)
+    })
+})
+
+// PUT ROUTE (edit todo)
+router.put('/edittodo/:id', (req, res) => {
+  const id = req.params.id
+  const todo = req.body
+  db.editTodo(id, todo)
+    .then(todo => {
+    /* eslint-disable no-console */
+      console.log('sending back edited todo')
+      res.json({todo})
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' +
+      err.message)
+    })
+})
+
+module.exports = router
