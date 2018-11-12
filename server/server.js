864605f157972eee302c795351ce449066ef0aeb
Lauren Hart <laurenannehart@gmail.com>
2018-11-13 12:47:40 +1300

    edited server.js to face routes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const todo = require('./routes/todo')
+
 const server = express()
 server.use(express.static(path.join(__dirname, 'public')))
 server.use(bodyParser.json())
 
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+server.use('/api/v1/todo', todo)
+
+// server.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// })
 
 module.exports = server
